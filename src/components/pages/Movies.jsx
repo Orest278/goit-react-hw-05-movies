c6a028f8144bcbbaa11db9d702813f47eb97da5e
Orest278 <125860863+Orest278@users.noreply.github.com>
2023-06-13 22:37:45 +0300

    run

--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -1,60 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import ListMovies from './ListMovie/ListMovie';
 import { searchMovies } from '../API/movies-api';
 
-export const Movies = () => {
-  const [query, setQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const handleSearch = async () => {
-    const movies = await searchMovies(query);
-    setSearchResults(movies);
-  };
-
-  const handleChange = (e) => {
-    setQuery(e.target.value);
-  };
+const Movies = () => {
+  const [movies, setMovies] = useState(null);
+  const [query, setQuery] = useSearchParams();
 
   useEffect(() => {
-    const urlSearchParams = new URLSearchParams(location.search);
-    const queryParam = urlSearchParams.get('query');
-    if (queryParam) {
-      setQuery(queryParam);
-      handleSearch();
+    const strQuery = query.get('query');
+    if (strQuery) searchMovies(strQuery).then(setMovies);
+    if (!strQuery) {
+      setMovies(null);
+      setQuery({});
     }
-  }, [location.search, handleSearch]);
-
-  const updateURL = () => {
-    const urlSearchParams = new URLSearchParams();
-    urlSearchParams.set('query', query);
-    navigate(`/movies?${urlSearchParams.toString()}`);
-  };
+  }, [query, setQuery]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleSearch();
-    updateURL();
-  };
+    setQuery({
+      query: e.currentTarget.elements.q.value,
+    });
+    e.currentTarget.reset();
+
+  }
 
   return (
     <div>
-      <h2>Search Movies</h2>
+       <h2>Search Movies</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={query} onChange={handleChange} />
-        <button type="submit">Search</button>
-      </form>
-
-      <ul>
-        {searchResults.map((movie) => (
-          <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <label>
+        <input name="q" type="text" />
+      </label>
+      <button type="submit">Search</button>
+      {movies?.length > 0 && (
+        <>
+          <ListMovies list={movies} />
+        </>
+      )}
+    </form>
     </div>
+    
   );
 };
 
-export default Movies;
+export default Movies
